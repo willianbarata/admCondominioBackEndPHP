b91e736fd19c5211eff7e26c12fa2d05c2eb0ac4
willianbarata <willianbarata@gmail.com>
2022-02-09 20:21:17 -0300

    Criando models e configurando

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,34 +13,10 @@ class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+    public $timestamps = false;
+    
     protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
+        'password'
     ];
 
     public function getJWTIdentifier()
