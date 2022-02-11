8f0591aad67fc17961cc4cba35aa5033d0174e76
willianbarata <willianbarata@gmail.com>
2022-02-10 22:39:11 -0300

    Informações do mural + like

--- app/Http/Controllers/WallController.php
+++ app/Http/Controllers/WallController.php
@@ -4,7 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\User;
+use App\Models\Wall;
+use App\Models\WallLike;
+
 class WallController extends Controller
 {
-    //
+    public function getAll(){
+        $array = ['error' => '','list' => []];
+
+        $user = auth()->user();
+
+        $walls = Wall::all();
+
+        foreach($walls as $wallkey => $wallvalue){
+            $walls[$wallkey]['likes'] = 0;
+            $walls[$wallkey]['liked'] = false;
+
+            $likes = WallLike::where('id_wall', $wallValue['id'])->count();
+            $walls[$wallkey]['likes'] = $likes;
+
+            $meLikes = WallLike::where('id_wall', $wallValue['id'])
+                ->where('id_user', $user['id'])
+                ->count();
+
+            if($meLikes > 0){
+                $walls[$wallkey]['liked'] = true;
+            }
+        }
+
+        $array['list'] = $walls;
+
+        return $array;
+    }
 }
