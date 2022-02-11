796dffe370a44ae90865c4413fded4490766b1d5
willianbarata <willianbarata@gmail.com>
2022-02-10 21:35:40 -0300

    Login finalizado

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -70,4 +70,44 @@ class AuthController extends Controller
 
         return $array;
     }
+
+    public function login(Request $request){
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(),[
+            'cpf' => 'required|digits:11',
+            'password' => 'required'
+        ]);
+
+        if(!$validator->fails()){
+            $cpf = $request->input('cpf');
+            $password = $request->input('password');
+
+            $token = auth()->attempt([
+                'cpf' => $cpf,
+                'password' => $password 
+            ]);
+
+            if(!$token){
+                $array['error'] = 'Cpf e/ou Senha estão errados.';
+                return $array;
+            }
+
+            $array['token'] = $token;
+            
+            $user = auth()->user();
+            $array['user'] = $user;
+
+            $properties = Unit::select(['id', 'name'])
+            ->where('id_owner', $user['id'])
+            ->get();
+
+            $array['user']['properties'] = $properties;
+        }else{
+            $array['error'] = $validator->errors()->first();
+            return $array;
+        }
+
+        return $array;
+    }
 }
