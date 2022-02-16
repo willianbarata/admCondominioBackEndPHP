1e6ec031345f14731d47be732c3cc25b34a5bb5d
willianbarata <willianbarata@gmail.com>
2022-02-16 20:57:06 -0300

    Lógica da reserva

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -11,12 +11,28 @@ class ReservationController extends Controller
     
     public function getReservations(){
         $array = ['error' => ''];
+        $daysHelper = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
 
         $areas = Area::where('allowed', 1)->get();
 
         $array['list'] = $areas;
 
-        
+        foreach($areas as $area){
+            $dayList = explode(',', $area['days']);
+
+            $dayGroups = [];
+
+            $lastDay = intval(current($dayList));
+            $dayGroups[] = $daysHelper[$lastDay];
+
+            array_shift($dayList);
+
+            $dayGroups[] = $daysHelper[end($dayList)];
+
+            echo "Area: ".$area['title']."\n";
+            print_r($dayGroups);
+            echo "\n----------";
+        }
 
         return $array;
     }
