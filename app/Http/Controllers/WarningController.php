c9f876bb878fbbfb2ba8b11127b9f0531dd4b07b
willianbarata <willianbarata@gmail.com>
2022-02-15 21:37:05 -0300

    envio de foto para ocorrência via post

--- app/Http/Controllers/WarningController.php
+++ app/Http/Controllers/WarningController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 use App\Models\Warning;
 use App\Models\Unit;
@@ -50,6 +52,25 @@ class WarningController extends Controller
             }
         }else{
             $array['error'] = 'A propriedade é necessária';
+        }
+
+        return $array;
+    }
+
+    public function addWarningFile(Request $request){
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(), [
+            'photo' => 'required|file|mimes:jpg,png'
+        ]);
+
+        if(!$validator->fails()){
+            $file = $request->file('photo')->store('public');
+
+            $array['photo'] = asset(Storage::url($file));
+        }else{
+            $array['error'] = $validator->errors()->first();
+            return $array;
         }
 
         return $array;
