9378cff4a84c162e1cec96b09595cfeb23fc90bc
willianbarata <willianbarata@gmail.com>
2022-02-15 22:13:55 -0300

    lista de perdidos e achados

--- app/Http/Controllers/FoundAndLostController.php
+++ app/Http/Controllers/FoundAndLostController.php
@@ -4,7 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\FoundAndLost;
+
 class FoundAndLostController extends Controller
 {
-    //
+    
+    public function getAll(){
+        $array = ['error' => ''];
+
+        $lost = FoundAndLost::where('status', 'LOST')
+            ->orderBy('datecreated', 'DESC')
+            ->orderBy('id', 'DESC')
+            ->get();
+
+        $recovered = FoundAndLost::where('status', 'RECOVERED')
+            ->orderBy('datecreated', 'DESC')
+            ->orderBy('id', 'DESC')
+            ->get();
+
+
+            foreach($lost as $lostKey => $lostValue){
+                $lost[$lostKey]['datecreated'] = date('d/m/Y', strtotime($lostValue['datecreated']));
+                $lost[$lostKey]['photo'] = asset('storage/'.$lostValue['photo']);
+            }
+
+            foreach($recovered as $recKey => $recValue){
+                $recovered[$recKey]['datecreated'] = date('d/m/Y', strtotime($recValue['datecreated']));
+                $recovered[$recKey]['photo'] = asset('storage/'.$recValue['photo']);
+            }
+
+            $array['lost'] = $lost;
+            $array['recovered'] = $recovered;
+
+        return $array;
+    }
+
 }
