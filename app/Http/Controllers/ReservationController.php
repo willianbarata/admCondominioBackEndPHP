36be022544f22b6af14933c207cee11ecafe5051
willianbarata <willianbarata@gmail.com>
2022-02-16 20:45:14 -0300

    Get do Reservation

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -4,7 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Area;
+
 class ReservationController extends Controller
 {
-    //
+    
+    public function getReservations(){
+        $array = ['error' => ''];
+
+        $areas = Area::where('allowed', 1)->get();
+
+        $array['list'] = $areas;
+
+        
+
+        return $array;
+    }
+
 }
