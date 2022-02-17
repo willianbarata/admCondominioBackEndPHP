a07a6958447d659c8e4d66c580eb0e01b3bdd68f
willianbarata <willianbarata@gmail.com>
2022-02-16 21:54:19 -0300

    Lógica para organizar dias e horários

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -10,7 +10,7 @@ class ReservationController extends Controller
 {
     
     public function getReservations(){
-        $array = ['error' => ''];
+        $array = ['error' => '', 'list' => []];
         $daysHelper = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
 
         $areas = Area::where('allowed', 1)->get();
@@ -27,11 +27,46 @@ class ReservationController extends Controller
 
             array_shift($dayList);
 
+            foreach($dayList as $day){
+                if(intval($day) != $lastDay+1){
+                    $dayGroups[] = $daysHelper[$lastDay];
+                    $dayGroups[] = $daysHelper[$day];
+                }
+                $lastDay = intval($day);
+            }
+            
             $dayGroups[] = $daysHelper[end($dayList)];
 
-            echo "Area: ".$area['title']."\n";
-            print_r($dayGroups);
-            echo "\n----------";
+            $dates = '';
+            $close = 0;
+            foreach($dayGroups as $group){
+                if($close === 0){
+                    $dates .= $group;
+                }else{
+                    $dates .= '-'.$group.',';
+                }
+
+                $close = 1 - $close;
+            }
+
+            $dates = explode(',', $dates);
+            array_pop($dates);
+
+            $start = date('H:i', strtotime($area['start_time']));
+            $end = date('H:i', strtotime($area['end_time']));
+
+            foreach($dates as $dKey => $dValue){
+                $dates[$dKey] .= ' '.$start.' às '.$end;
+            }
+
+            $dayGroups[] = $daysHelper[end($dayList)];
+
+            $array['list'][] = [
+                'id' => $area['id'],
+                'cover' => asset('storage/'.$area['cover']),
+                'title' => $area['title'],
+                'dates' => $dates
+            ];
         }
 
         return $array;
